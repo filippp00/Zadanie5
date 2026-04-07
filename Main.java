18e5e8541af77cfced40258b6c33646a2c5bab86
filippp00 <filip.m.maj@icloud.com>
2026-04-07 20:46:46 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,5 +17,15 @@ public class Main {
         // Na koniec test Agenta (z Części 1)
         System.out.println("\nTest Agenta:");
         new Agent("James", "Bond").przedstawSie();
+        System.out.println("\n--- Testy Arytmetyki Ułamków ---");
+        Ulamek u1 = new Ulamek(1, 2);
+        Ulamek u2 = new Ulamek(1, 4);
+
+        System.out.println("Dodawanie: " + u1 + " + " + u2 + " = " + u1.dodaj(u2));
+        System.out.println("Odejmowanie: " + u1 + " - " + u2 + " = " + u1.odejmij(u2));
+        System.out.println("Mnożenie: " + u1 + " * " + u2 + " = " + u1.pomnoz(u2));
+        System.out.println("Dzielenie: " + u1 + " / " + u2 + " = " + u1.podziel(u2));
+        System.out.println("Dodanie liczby 2 do " + u1 + " = " + u1.dodajLiczbe(2));
+        System.out.println("Test ujemnego mianownika (1, -2): " + new Ulamek(1, -2));
     }
 }
